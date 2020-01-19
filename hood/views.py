08913178898388c1f941cf25be4bb7f39e338b26
wilbrone <wilbroneokoth@gmail.com>
2020-01-19 20:41:36 +0300

    add forms for signup

--- hood/views.py
+++ hood/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm
 from django.contrib.auth import login, authenticate
-from .models import *
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.views.generic import RedirectView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+
+from .forms import *
+from .models import *
 
 # Create your views here.
 
