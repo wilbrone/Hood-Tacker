5d651aa355a229ef08eede7ffa0a2773a05f0301
wilbrone <wilbroneokoth@gmail.com>
2020-01-20 05:18:44 +0300

    add tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from . models import *
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+
+        self.new_profile=Profile(bio='i love basketball')
+    # test for instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    # testing the save mothod
+    def test_save_profile(self):
+        self.new_profile.create_profile()
+        profile=Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+class NeighborhoodTestClass(TestCase):
+
+        def setUp(self):
+            self.new_neighborhood=Neighborhood(name='killeleshwa',population=20101000)
+        def tearDown(self):
+            Neighborhood.objects.all().delete()
+
+
+        # test for instance
+        def test_instance(self):
+            self.assertTrue(isinstance(self.new_neighborhood,Neighborhood))
+        # test for save method
+        def test_save_neighborhood(self):
+            self.new_neighborhood.create_neighborhood()
+            neighborhood=Neighborhood.objects.all()
+            self.assertTrue(len(neighborhood)>0)
+        def test_delete_neighborhood(self):
+            self.new_neighborhood.create_neighborhood()
+            self.new_neighborhood.delete_neighborhood()
+            neighborhood=Neighborhood.objects.all()
+            self.assertEqual(len(neighborhood),0)
